45c7c68fbc469263fcc8c39406915b5aa20c19e6
Marci <128537619+marci1175@users.noreply.github.com>
2023-07-22 10:30:00 +0200

    tonic echo server

---
+++ src/client.rs
@@ -0,0 +1,30 @@
+use hello_world::greeter_client::GreeterClient;
+use hello_world::HelloRequest;
+use std::io;
+use std::os::windows::thread;
+use std::time::Duration;
+pub mod hello_world {
+    tonic::include_proto!("helloworld");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    loop {
+        println!("Enter something");
+        let mut input = Default::default();
+        io::stdin().read_line(&mut input).expect("failed to readline");
+        let request = tonic::Request::new(HelloRequest {
+            name: input.trim().into(),
+        });
+        let response = match client.say_hello(request).await{
+            Ok(ok) => {ok},
+            Err(_) => {break;}
+        };
+
+        println!("{:?}", response);
+    }
+    println!("bye bye!");
+    std::thread::sleep(Duration::from_secs(10));
+    Ok(())
+}
